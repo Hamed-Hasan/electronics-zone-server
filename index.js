5502a24e8c0e01defa4409b8676d508a25d2c9a8
John Doe <hamedhasan422421@gmail.com>
2022-04-29 14:42:02 +0300

    inventory service added

--- index.js
+++ index.js
@@ -22,8 +22,20 @@ async function run() {
         const serviceCollection = client.db('electronics').collection('service');
         const orderCollection = client.db('electronics').collection('order');
 
-      
-     
+    //   show display service
+     app.get('/service', async (req, res) => {
+         const query = {};
+         const cursor = serviceCollection.find(query);
+         const result = await cursor.toArray()
+         res.send(result);
+     })
+
+     app.get('/service/:id', async (req, res) => {
+        const id = req.params.id;
+        const query={_id: ObjectId(id)}
+        const service = await serviceCollection.findOne(query);
+        res.send(service);
+    })
 
     }
     finally {
